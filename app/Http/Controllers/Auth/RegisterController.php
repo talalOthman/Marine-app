115dd4de82bec2537a2358da782b09bfecadfdaa
Talal <talalothmanse@gmail.com>
2022-03-01 21:12:48 +0800

    -Minor fixes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -38,7 +38,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('auth');
     }
 
     /**
@@ -50,8 +50,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'userName' => ['required', 'string', 'max:255', 'unique:users'],
+            'userType' => ['required', 'string', 'max:255'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -65,8 +65,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'userName' => $data['userName'],
+            'userType' => $data['userType'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('userName')->unique();
+            $table->string('userType');
             $table->string('password');
             $table->boolean('has_image')->default(false);
             $table->string('avatar')->default('default.png');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
